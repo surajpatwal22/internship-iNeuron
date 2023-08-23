a077a0183949af2d14e6fd81e8440bd32fbb22e4
surajpatwal22 <patwal0suraj@gmail.com>
2023-08-23 14:31:06 +0530

    weatherapp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './App.css'
+import Weather from './assets/Weather'
+
+function App() {
+
+  return (
+    <>
+    <Weather/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Weather.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+import './weather.css'
+
+const Weather = () => {
+    const [weatherdata, setWeatherData] = useState(null);
+    const [City,setCity] = useState("dehradun");
+    const [error , setError] = useState(null);
+    // const Apikey = "8bebb3b8a6bd448d90a52501231908";
+    
+    
+    useEffect(() => {
+        if (City) {
+            fetchWeatherData();
+        }    
+    }, [City])
+
+    const fetchWeatherData = async () =>{
+        setError(null);
+        await fetch(`https://api.weatherapi.com/v1/current.json?key=8bebb3b8a6bd448d90a52501231908&q=${City}`)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data);
+                if (data && data.location && data.current) {
+                    setWeatherData(data);
+                    console.log(data);
+                } else {
+                    console.error("unexpected api responce");
+                }
+            })
+            .catch((error)=>{
+                console.error('fetching error',error);
+                // setWeatherData(null);
+                setError('An error occured please try again later')
+
+            })
+    }
+    // const {condition, temp_c, wind_kph , humidity ,uv ,last_updated} = weatherdata.current;
+    // const {name , region , country , localtime} = weatherdata.location;
+
+    const {location ,current} = weatherdata || {};
+
+    return (
+        <div className='main-container'>
+            <div className='input-sxn'>
+                <input className='input-feild' type="text" placeholder='enter city name' defaultValue={City}  onClick={(e)=>{
+                    setCity(e.target.value)    
+                }} />
+                <button className='search-btn' onClick={fetchWeatherData}>Search</button>
+            </div>
+           <div className='second-sxn'>
+           <div className='iconsection display-box '>
+                <img className='ig1' src={current?.condition?.icon} alt="conditionicon" />
+                <h2 className='hd'>{current?.condition?.text}</h2>
+            </div>
+            <div className='loaction-sxn display-box'>
+                <h2>Place : {location?.name}({location?.region})</h2>
+                <h2>Country : {location?.country} </h2>
+            </div>
+            <div className='time-sxn display-box'>
+                <h2>Date&Time: <br /> <br />{location?.localtime}</h2>
+            </div>
+           </div>
+           <div className='third-sxn'>
+            <h2 className='display-box hd1'>Humidity:<hr />  {current?.humidity}</h2>
+            <h2 className='display-box hd2'>Temp: <hr /> {current?.temp_c}degree celcius</h2>
+            <h2 className='display-box hd3'>Wind-Speed: <hr /> {current?.wind_kph}kph</h2>
+                
+           </div>
+            
+        </div>
+    )
+}
+
+export default Weather
